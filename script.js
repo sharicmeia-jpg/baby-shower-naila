bd35f7f5a1c7aecec3ba9879c7eb35af4b93498f
sharicmeia-jpg <sharic.meia@cun.edu.co>
2026-06-16 10:19:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function entrar() {
-    window.location.href = "juego.html";
-}
+const premios = ["🧸", "🍼", "🎀", "🍭", "💝", "🧁", "🎈", "👶"];
+
+box.addEventListener("click", () => {
+  const premio = premios[Math.floor(Math.random() * premios.length)];
+
+  box.innerText = premio;
+  box.style.background = "#ff4d88";
+  box.style.color = "white";
+  box.style.transform = "scale(1.1)";
+  box.style.transition = "0.3s";
+  box.style.pointerEvents = "none";
+});
